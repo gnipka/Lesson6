e7fc5f8f98af66525b13d729f1da66e87d68cb69
gnipka <gnipka29@gmail.com>
2022-12-02 19:54:11 +0300

    Добавьте файлы проекта.

---
+++ Lesson6/Sample01.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.DAL;
+
+internal class Sample01
+{
+    static void Main(string[] args)
+    {
+        var dbContextOptionsBuilder = new DbContextOptionsBuilder<OrdersDbContext>()
+            .UseSqlServer("data source=; initial catalog=OrdersDatabase;User Id=OrdersUser;Password=12345;");
+            // data source = Имя сервера
+    }
+}
